6f2d4de72baaa439902185b02e1022d6ae9e0092
Fredholm <41293039+HolmDev@users.noreply.github.com>
2022-11-01 19:30:36 +0100

    Fixed OOR error and removed unneccesary newline

--- main.go
+++ main.go
@@ -11,11 +11,13 @@ import (
 
 
 func main() {
-	if os.Args[1] == "-h" || os.Args[1] == "--help"{
-		fmt.Println("huecat v1.0. Colorify your command output!")
-		fmt.Println("usage: huecat [-h]");
-		fmt.Println("example: echo \"#ffffff\" | huecat");
-		os.Exit(0);
+	if len(os.Args) > 1 {
+		if os.Args[1] == "-h" || os.Args[1] == "--help" {
+			fmt.Println("huecat v1.0. Colorify your command output!")
+			fmt.Println("usage: huecat [-h]");
+			fmt.Println("example: echo \"#ffffff\" | huecat");
+			os.Exit(0);
+		}
 	}
 
 	reader := bufio.NewReader(os.Stdin);
@@ -35,5 +37,5 @@ func main() {
 
 	hex24bit := regexp.MustCompile(`#([0-9A-Fa-f]){6}`);
 	colorified := hex24bit.ReplaceAllStringFunc(input, colorify);
-	fmt.Println(colorified);
+	fmt.Printf(colorified);
 }
